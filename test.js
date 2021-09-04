58990f6c2187891559986028ef87fe32123f3515
iamrosada <luisrosada2018@gmail.com>
2021-09-04 12:57:30 +0300

    split and trim

--- test.js
+++ test.js
@@ -1,21 +1,32 @@
+//split() and trim()
+let anyword = "     Javascript language is the best the than Java      ";
+
+let testSplit = anyword.split("");
+
+let testTrim = anyword.trim();
+
+console.log(testSplit);
+
+console.log(testTrim);
+
 //replace and slice
 
 let text = "Javascript language is the best the than Java";
 
 let testingTheReplace = text.replace("Java", "any language");
 let testingTheReplaceInGlogal = text.replace(/Java/g, "Jesus");
-console.log(testingTheReplace);
+/* console.log(testingTheReplace);
 console.log(testingTheReplaceInGlogal);
-
-console.log("\n");
+ */
+/* console.log("\n"); */
 
 let university = "Javascript language is the best the than Java";
 
 let tamanho = university.length;
 
 let testingSlice = university.slice(0, tamanho);
-
-console.log(testingSlice);
+/* 
+console.log(testingSlice); */
 
 /* const family = ["luis", "filipe", "vasco", "evanilson"];
 
